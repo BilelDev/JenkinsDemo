093f0cb33b17aa64fb0af13019e7b134b49327f2
Bilel Daikhi <bilel.daikhi@gmail.com>
2020-11-03 10:38:41 +0100

    this pull will be transfered to jenkins automatically

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 public static void main(String[] args){
 System.out.println("hello world!!");
 System.out.println("modification pushed from my desktop!!");
+System.out.println("jenkins will pull it automatically from git hub!!");
 }
 }
